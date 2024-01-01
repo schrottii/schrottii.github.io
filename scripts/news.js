a3ae0cb30b17999a8a5867857e078fb01e5fe3b3
fn78 <oh.seyy@yahoo.de>
2024-01-01 11:17:54 +0100

    forgot the news oops

--- scripts/news.js
+++ scripts/news.js
@@ -28,6 +28,7 @@ const news = [
     new News(1, "Host issues solved!", "My games are now hosted via Github. All links on this site have been updated."),
     new News(2, "Shgabb Clicker v2.1.2", "Some balancing, GS changes and many nice small changes!"),
     new News(3, "Website v1.0", "My website is no longer WIP! Now with news, about me and more!"),
+    new News(4, "SC2FMFR v3.3.1", "December 31st - A new SC2FMFR update is out now! Just 2 hours before the new year begins in Europe... I hope you enjoy this update as much as I do, it's one of my favorite FR updates! It adds 2 cool new settings, makes tree upgrades cheaper, fixes bugs and more!"),
 ]
 
 renderNews();
